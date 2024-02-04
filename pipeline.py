6e5f28b5f18c3e7fcf5a2377e8d0c5dd827f3b2d
MonTheDog <cate.party@gmail.com>
2024-02-04 16:26:55 +0100

    First commit

---
+++ pipeline.py
@@ -0,0 +1,15 @@
+# Data Understanding and Preparation (Graphs, Summary Statistics, Data Cleaning, Data Transformation, Feature Engineering)
+
+
+
+# Modeling (Training, Testing, Optimization)
+
+
+
+# Evaluation (Accuracy, Precision, Recall, F1, AUC, ROC, Confusion Matrix)
+
+
+
+# Deployment (Small web app using streamlit)
+
+
